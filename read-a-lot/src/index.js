02e7eac22bf7fc4466ecde096594f74ced9e1a59
Malik <willis.malik.r@gmail.com>
2023-08-09 12:26:01 -0500

    routes complete

--- read-a-lot/src/index.js
+++ read-a-lot/src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import App from './App';
+import * as ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>
+    },
+    {
+        path: '/favorites',
+        element: <Favorites/>
+    }
+    ])
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
+      <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
