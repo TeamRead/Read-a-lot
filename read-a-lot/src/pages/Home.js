9f107da916eec40323089697116411cbb942fa19
Penelope <phuff110@gmail.com>
2023-08-23 11:38:03 -0500

    carousel displays 3 random books from selected subject

--- read-a-lot/src/pages/Home.js
+++ read-a-lot/src/pages/Home.js
@@ -7,69 +7,72 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 
 function Home() {
-  const [subjectData, setSubjectData] = useState();
-  const [coverId, setCoverId] = useState();
-  async function fetchBooks(){
-    const response = await fetch(`http://openlibrary.org/subjects/love.json`);
-    const subData = await response.json();
-    console.log(subData)
-    setSubjectData(subData);
+    const [subjectData, setSubjectData] = useState([]);
+    const [randomBooks, setRandomBooks] = useState([]);
+    
+    async function fetchBooks() {
+      const response = await fetch(`http://openlibrary.org/subjects/love.json`);
+      const subData = await response.json();
+      setSubjectData(subData.works || []);
+    }
+  
+    useEffect(() => {
+      fetchBooks();
+    }, []);
+  
+    useEffect(() => {
+      if (subjectData.length > 0) {
+        const randomIndices = [];
+        while (randomIndices.length < 3) {
+          const randomIndex = Math.floor(Math.random() * subjectData.length);
+          if (!randomIndices.includes(randomIndex)) {
+            randomIndices.push(randomIndex);
+          }
+        }
+        const selectedBooks = randomIndices.map(index => subjectData[index]);
+        setRandomBooks(selectedBooks);
+      }
+    }, [subjectData]);
+  
 
-  }
-  useEffect(() => {
-    fetchBooks().then(r => r);
-  }, []);
   return (
   <>
     <div className="title">
     
     <h1>HOME</h1>
+      </div>
 
-    </div>
+      <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Romance</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Horror</Dropdown.Item>
+      </DropdownButton>
 
-    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-      <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-      <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-    </DropdownButton>
-
-
-    <Container>
-      <Row>
-        <Col>
-        <div className="card">
-            <img className="card-img-top w-25" src="https://covers.openlibrary.org/b/id/12547191-L.jpg" alt="Card image cap" />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-        </Col>
-        <Col>
-        <div className="card">
-            <img className="card-img-top w-25" src="https://covers.openlibrary.org/b/id/12547191-L.jpg" alt="Card image cap" />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-        </Col>
-        <Col>
-        <div className="card">
-            <img className="card-img-top w-25" src="https://covers.openlibrary.org/b/id/12547191-L.jpg" alt="Card image cap" />
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-        </div>
-        </Col>
-      </Row>
-    </Container>
-
-</>
+      <Container>
+        <Row>
+          {randomBooks.map((book, index) => (
+            <Col key={index}>
+              <div className="card">
+                <img
+                  className="card-img-top w-100 h-auto"
+                  src={`https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`}
+                  alt="Card cover"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{book.title}</h5>
+                  <p className="card-text">
+                    {book.authors ? book.authors.map(author => author.name).join(', ') : 'Unknown Author'}
+                  </p>
+                  <a href="#" className="btn btn-primary">
+                    Add to My Library
+                  </a>
+                </div>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 }
 
