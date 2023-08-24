1b06ad9503f06e0957fd4ced3d248068274e254f
Malik <willis.malik.r@gmail.com>
2023-08-23 22:08:42 -0500

    favorite book refresh with books that wasn't selected

--- read-a-lot/src/pages/Home.js
+++ read-a-lot/src/pages/Home.js
@@ -11,10 +11,13 @@ function Home() {
     const [subjectData, setSubjectData] = useState([]);
     const [randomBooks, setRandomBooks] = useState([]);
     const { addFavorite } = useContext(FavoritesContext);
+    const [refreshSameBooks, setrEfreshSameBooks] = useState([]);
+    const [newRandomBooks, setNewRandomBooks] = useState([]);
     
     async function fetchBooks() {
       const response = await fetch(`https://openlibrary.org/subjects/love.json`);
       const subData = await response.json();
+        console.log(subData.works)
       setSubjectData(subData.works || []);
     }
   
@@ -35,6 +38,48 @@ function Home() {
         setRandomBooks(selectedBooks);
       }
     }, [subjectData]);
+
+    function addToFavoritesRefresh(bookName){
+        for(let i =0; i < randomBooks.length; ++i){
+            if(bookName !== randomBooks[i].title){
+                refreshSameBooks.push(randomBooks[i].title)
+            }
+        }
+        // async function fetchBooks() {
+        //     const response = await fetch(`https://openlibrary.org/subjects/love.json`);
+        //     const subData = await response.json();
+        //     setSubjectData(subData.works || []);
+        // }
+        // fetchBooks().then(r => r);
+
+        const randomIndices = [];
+        while (randomIndices.length < 1) {
+            const randomIndex = Math.floor(Math.random() * subjectData.length);
+            if (!randomIndices.includes(randomIndex)) {
+                randomIndices.push(randomIndex);
+            }
+        }
+        const selectedBooks = randomIndices.map(index => subjectData[index]);
+        refreshSameBooks.push(selectedBooks[0].title);
+
+
+        async function fetchRefreshBooks() {
+            setRandomBooks([])
+            for(let i = 0; i < refreshSameBooks.length; ++i ){
+                randomBooks.pop()
+                const response = await fetch(`https://openlibrary.org/search.json?title=${refreshSameBooks[i]}`);
+                const subData = await response.json();
+                newRandomBooks.push(subData.docs[0])
+
+            }
+            setRandomBooks(newRandomBooks)
+            console.log(randomBooks)
+        }
+        fetchRefreshBooks().then(r => r);
+        setRandomBooks([])
+        console.log(randomBooks)
+
+    }
   
 
   return (
@@ -57,7 +102,7 @@ function Home() {
               <div className="card">
                 <img
                   className="card-img-top w-100 h-auto"
-                  src={`https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`}
+                  src={`https://covers.openlibrary.org/b/id/${book.cover_id || book.cover_i}-L.jpg` }
                   alt="Card cover"
                 />
                 <div className="card-body">
@@ -65,7 +110,7 @@ function Home() {
                   <p className="card-text">
                     {book.authors ? book.authors.map(author => author.name).join(', ') : 'Unknown Author'}
                   </p>
-                  <button onClick={() => addFavorite(book)}>
+                  <button onClick={()=> addToFavoritesRefresh(book.title)}>
                     Add to My Library
                   </button>
                 </div>
